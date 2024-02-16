49cc5742dbb07abda47b01e5d849af0c70499cf1
Kye <kye@apacmediasolutions.com>
2024-02-15 20:25:50 -0800

    [EXAMPLE]

--- sora_torch/model.py
+++ sora_torch/model.py
@@ -1,4 +1,32 @@
+import torch 
 from torch import nn, Tensor
+from einops import rearrange
+
+
+def patchify_videos(
+    x: Tensor,
+    patch_size: tuple = (16, 16, 16),
+):
+    """
+    Rearranges a video tensor into patches of specified size.
+
+    Args:
+        x (Tensor): The input video tensor of shape (batch_size, channels, depth, height, width).
+        patch_size (tuple, optional): The size of each patch in the format (depth, height, width). Defaults to (16, 16, 16).
+
+    Returns:
+        Tensor: The rearranged video tensor of shape (batch_size * num_patches, channels, patch_depth, patch_height, patch_width).
+    """
+    d, h, w, = patch_size
+    
+    # Rearrange the video tensor into patches
+    patched_videos = rearrange(
+        x,
+        "b c (d pd) (h ph) (w pw) -> (b d h w) c pd ph pw",
+        pd=d, ph=h, pw=w
+    )
+    
+    return patched_videos
 
 
 class Sora(nn.Module):
